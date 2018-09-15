c88911682cac2447a6e048f17b74e6806c9426bb
Ansgar Grunseid <grunseid@gmail.com>
2018-09-14 19:49:29 -0700

    Note Python 2 compatibility in setup.py.

--- setup.py
+++ setup.py
@@ -89,6 +89,7 @@ setup(
         'Topic :: Software Development :: Libraries',
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
