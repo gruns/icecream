5b3c48357c5dbb9dce6cac5f4e47f0ff95656c79
Ansgar Grunseid <grunseid@gmail.com>
2020-02-27 14:53:56 -0800

    Add install() and uninstall() tests.

---
+++ tests/install_test_import.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+#
+# IceCream - A little library for sweet and creamy print debugging.
+#
+# Ansgar Grunseid
+# grunseid.com
+# grunseid@gmail.com
+#
+# License: MIT
+#
+
+def runMe():
+    x = 3
+    ic(x)
