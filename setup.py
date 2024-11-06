5a7983983b2346813d03c2bca8264ef713997137
Ansgar Grunseid <grunseid@gmail.com>
2024-11-06 12:09:40 -0800

    drop support for python 3.5, which hit EOL in september 2020

--- setup.py
+++ setup.py
@@ -87,7 +87,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
